1ac491aa37fe6c4ff84890ac38653f74188372c9
Sujith Mogallapalli <sujithmogallapalli@gmail.com>
2026-02-21 11:49:37 +0530

    2. Limits service - Using application.properties config values

--- limits-service/src/main/java/com/learning/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/learning/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.learning.limitsservice.controller;
 
 import com.learning.limitsservice.bean.Limits;
+import com.learning.limitsservice.configuration.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
