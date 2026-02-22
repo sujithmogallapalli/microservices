64ef7d3421355ed6f450b4d25e94ba3bd699bf1d
Sujith Mogallapalli <sujithmogallapalli@gmail.com>
2026-02-22 20:44:13 +0530

    Currency Conversion service - hard coded values

---
+++ currency-conversion-service/src/main/java/com/learning/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,19 @@
+package com.learning.currencyconversionservice.controller;
+
+import com.learning.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+
+}
