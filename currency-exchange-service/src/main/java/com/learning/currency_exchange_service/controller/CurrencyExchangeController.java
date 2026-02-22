674ef29d58baf548aa1017672f3fd119d28bc9e2
Sujith Mogallapalli <sujithmogallapalli@gmail.com>
2026-02-22 17:21:28 +0530

    Currency Exchange service - using in memory db with H2

--- currency-exchange-service/src/main/java/com/learning/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -1,8 +1,7 @@
 package com.learning.currency_exchange_service.controller;
 
-import java.math.BigDecimal;
-
 import com.learning.currency_exchange_service.model.CurrencyExchange;
+import com.learning.currency_exchange_service.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +14,16 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if (currencyExchange == null)
+            throw new RuntimeException("Unable to Find data for " + from + " to " + to);
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
---
+++ currency-exchange-service/src/main/java/com/learning/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.learning.currency_exchange_service.repository;
+
+import com.learning.currency_exchange_service.model.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
