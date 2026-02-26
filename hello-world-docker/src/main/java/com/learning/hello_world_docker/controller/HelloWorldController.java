6c5b6b83c8e95cc7cceb9ac3cd45a348c4ba234c
Sujith Mogallapalli <sujithmogallapalli@gmail.com>
2026-02-26 10:29:27 +0530

    Docker - Simple hello world with docker layer caching

--- hello-world-docker/src/main/java/com/learning/hello_world_docker/controller/HelloWorldController.java
+++ hello-world-docker/src/main/java/com/learning/hello_world_docker/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
     @GetMapping(path = "/")
     public String helloWorld() {
         return """
-                { message: Hello World Java v2 }
+                { message: Hello World Java v3 }
                """;
     }
 
