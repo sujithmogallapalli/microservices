568e73ec59017a942bea22de1b5a6a0a01b14f3d
Sujith Mogallapalli <sujithmogallapalli@gmail.com>
2026-02-26 09:30:41 +0530

    Docker - Simple hello world with manual build in local

---
+++ hello-world-docker/src/test/java/com/learning/hello_world_docker/HelloWorldDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hello_world_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
